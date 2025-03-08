afcc2d142ecb50fa058e7da02ae769220d053201
roum02 <roum02@ewhain.net>
2025-03-09 01:47:02 +0900

    Add event page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return <div>this is a main page</div>;
+  return (
+    <div>
+      <p>welcome to the main page. click the link below!</p>
+      <ul>
+        <li>
+          <Link href={"/event"}>going to the event page</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
