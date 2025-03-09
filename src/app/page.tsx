5f60295e38f14669941803c7c05c2a9dbe976b7a
이승원 <skmb1230@naver.com>
2025-03-10 01:45:41 +0900

    Modify push test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <p>CI/CD 배포 확인중..</p>
+      <p>CI/CD 배포 확인중.. push..</p>
       <ul>
         <li>
           <Link href={"/event"}>going to the event page</Link>
