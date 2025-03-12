0e59230d621d391393caf6cff869515eba18602f
이승원 <skmb1230@naver.com>
2025-03-12 21:43:07 +0900

    Modify test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <p>CI/CD 배포 확인중.. ESLint 수정</p>
+      <p>CI/CD 배포 확인중.. EC2 재부팅 테스트..</p>
       <ul>
         <li>
-          <Link href={'/event'}>going to the event page</Link>
+          <Link href={"/event"}>going to the event page</Link>
         </li>
         <li>
-          <Link href={'/event-history'}>going to the event history page</Link>
+          <Link href={"/event-history"}>going to the event history page</Link>
         </li>
       </ul>
     </div>
