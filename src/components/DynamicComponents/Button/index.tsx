65f0cdd8d011bfeef1915025390855242e38a8d9
이승원 <lsw4315@jobkorea.co.kr>
2025-03-25 14:02:53 +0900

    Add section type - 플로팅버튼, 커스텀 영역 추가

--- src/components/DynamicComponents/Button/index.tsx
+++ src/components/DynamicComponents/Button/index.tsx
@@ -1,12 +1,14 @@
+import { PageJsonContentsItem } from '@sd-ui-admin/types';
+
 export interface ButtonProps {
-  contents: { text?: string };
+  contents: PageJsonContentsItem;
   style?: React.CSSProperties;
 }
 
 export const Button = ({ contents, style }: ButtonProps) => {
   return (
     contents?.text && (
-      <button style={{ ...style }} aria-disabled>
+      <button style={contents.style} aria-disabled>
         {contents.text}
       </button>
     )
